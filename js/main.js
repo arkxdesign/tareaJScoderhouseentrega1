f85c5327098abed4b41efcb45c9287d71a259961
Adrian Cisneros <a_r_k_x@hotmail.com>
2023-10-12 17:38:45 -0600

    agregar if en la variable de miNombre

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
 var miCarrito = [];
-let miNombre = prompt("Ingresa tu nombre")
+let miNombre = prompt("Ingresa tu nombre");
 
-function calcularImpuesto(valorBase, impPaisMxIva = 0.16){
-    let impuestos = valorBase * impPaisMxIva;
-    alert("EL impuesto al valor agregado es de: " + impuestos + "MXN" + " '16% de IVA'")
-    return valorBase + impuestos
+if (miNombre == null || miNombre == "") {
+    miNombre = "Usuario Anónimo";
 }
 
-let confirmacion = true
+function calcularImpuesto(valorBase, impPaisMxIva = 0.16) {
+    let impuestos = valorBase * impPaisMxIva;
+    alert("EL impuesto al valor agregado es de: " + impuestos + "MXN" + " '16% de IVA'");
+    return valorBase + impuestos;
+}
+
+let confirmacion = true;
 do {
     let valorProducto = parseFloat(prompt("Ingrese el valor del producto"));
-        if( isNaN(valorProducto) || valorProducto <= 0 ){
-            valorProducto = parseFloat(0);
-            suma = 0;
-            alert("Ingresa un valor numerico mayor a 1, presiona F5 para reiniciar tu compra")
-            break
-        }
+    if (isNaN(valorProducto) || valorProducto <= 0) {
+        valorProducto = parseFloat(0);
+        suma = 0;
+        alert("Ingresa un valor numerico mayor a 1, presiona F5 para reiniciar tu compra");
+        break
+    }
 
-    let costoTotal = calcularImpuesto(valorProducto)
-    alert("El costo con impuesto es de: " + costoTotal + "MXN")
-   
+    let costoTotal = calcularImpuesto(valorProducto);
+    alert("El costo con impuesto es de: " + costoTotal + "MXN");
+
     miCarrito.push(costoTotal);
-    
+
     var suma = 0;
-    for (i = 0; i < miCarrito.length; i++){
+    for (i = 0; i < miCarrito.length; i++) {
         suma += miCarrito[i];
     }
-    
-    let respuesta = prompt(`¿Desea ingresar otro producto? escriba SI o NO cualquier otra respuesta diferente de NO se tomara como un SI`)
-    let respuesta2 = alert(`Llevas ${miCarrito.length} producto's y el total de tu compra al momento es de: ${suma} MXN`)
-        
-    if (respuesta == 'no' || respuesta == 'NO' || respuesta == 'No' || respuesta == 'nO'){
+
+    let respuesta = prompt(`¿Desea ingresar otro producto? escriba SI o NO cualquier otra respuesta diferente de NO se tomara como un SI`);
+    let respuesta2 = alert(`Llevas ${miCarrito.length} producto's y el total de tu compra al momento es de: ${suma} MXN`);
+
+    if (respuesta == 'no' || respuesta == 'NO' || respuesta == 'No' || respuesta == 'nO') {
         respuesta3;
         confirmacion = false;
     }
     respuesta2
 } while (confirmacion == true);
 
-var respuesta3 = alert(`Gracias ${miNombre} compraste ${miCarrito.length} producto's y el total de tu compra es de: ${suma} MXN`)
-console.log("Fin del ciclo");
-console.log(`Gracias ${miNombre} compraste ${miCarrito.length} producto's y el total de tu compra es de: ${suma} MXN`);
-
-
+var respuesta3 = alert(`Gracias ${miNombre} compraste ${miCarrito.length} producto's y el total de tu compra es de: ${suma} MXN`);
