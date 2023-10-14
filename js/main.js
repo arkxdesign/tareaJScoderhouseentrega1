9058373edc0370fa02384e4406efd8228c2b545f
Adrian Cisneros <a_r_k_x@hotmail.com>
2023-10-14 16:42:04 -0600

    Agregar miDireccion y validaciones con do while en SI o No al continuar con la compra

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 var miCarrito = [];
 let miNombre = prompt("Ingresa tu nombre");
-
 if (miNombre == null || miNombre == "") {
     miNombre = "Usuario Anónimo";
 }
+
+let validarDireccion = true;
+do {
+    var miDireccion = prompt("Ingresa tu dirección de envio");
+    if (miDireccion.length <= 9) {
+        alert("Ingresa un dirección de minimo 10 caracteres")
+    } else if (miDireccion.length >= 10) {
+        break;
+    }
+} while (validarDireccion == true);
 
 function calcularImpuesto(valorBase, impPaisMxIva = 0.16) {
     let impuestos = valorBase * impPaisMxIva;
@@ -11,34 +20,48 @@ function calcularImpuesto(valorBase, impPaisMxIva = 0.16) {
     return valorBase + impuestos;
 }
 
+function validarRespuesta(texto, textoValidar) {
+    if (texto.toLowerCase() == textoValidar) {
+        return textoValidar
+    }
+}
+
 let confirmacion = true;
 do {
-    let valorProducto = parseFloat(prompt("Ingrese el valor del producto"));
-    if (isNaN(valorProducto) || valorProducto <= 0) {
-        valorProducto = parseFloat(0);
-        suma = 0;
-        alert("Ingresa un valor numerico mayor a 1, presiona F5 para reiniciar tu compra");
-        break
-    }
+
+    let valorProducto
+    do {
+        valorProducto = parseFloat(prompt("Ingrese el valor del producto adquirido"))
+        if (isNaN(valorProducto) == true) {
+            alert("Solo se permiten numeros")
+        }
+    } while (isNaN(valorProducto) == true)
 
     let costoTotal = calcularImpuesto(valorProducto);
     alert("El costo con impuesto es de: " + costoTotal + "MXN");
 
     miCarrito.push(costoTotal);
-
     var suma = 0;
     for (i = 0; i < miCarrito.length; i++) {
         suma += miCarrito[i];
     }
 
-    let respuesta = prompt(`¿Desea ingresar otro producto? escriba SI o NO cualquier otra respuesta diferente de NO se tomara como un SI`);
-    let respuesta2 = alert(`Llevas ${miCarrito.length} producto's y el total de tu compra al momento es de: ${suma} MXN`);
+    let resp = true;
+    do {
+        let respuesta = prompt("¿Desea ingresar otro producto? escriba SI, o NO para terminar con su compra.");
+        if (validarRespuesta(respuesta, "no") == "no") {
+            alert(`${miNombre} compraste ${miCarrito.length} producto's y el total de tu compra es de: ${suma} MXN y tu pedido se enviara a ${miDireccion} Gracias por tu compra!`);
+            resp == false;
+            confirmacion = false;
+            break
+        } else if (validarRespuesta(respuesta, "si") == "si") {
+            alert(`Llevas ${miCarrito.length} producto's y el total de tu compra al momento es de: ${suma} MXN`);
+            resp == false;
+            confirmacion = true;
+            break
+        }
+        alert(`Respuesta invalida`);
 
-    if (respuesta == 'no' || respuesta == 'NO' || respuesta == 'No' || respuesta == 'nO') {
-        respuesta3;
-        confirmacion = false;
-    }
-    respuesta2
+    } while (resp == true)
+
 } while (confirmacion == true);
-
-var respuesta3 = alert(`Gracias ${miNombre} compraste ${miCarrito.length} producto's y el total de tu compra es de: ${suma} MXN`);
